85507325aa25db23acd79136e66013f5986a2e53
seoyunZZang <p815000@naver.com>
2024-12-01 02:58:32 +0900

    font 추가

--- frontend/App.js
+++ frontend/App.js
@@ -1,71 +1,27 @@
-// App.js -> 내비게이션으로 코듯 수정하기
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import 'react-native-gesture-handler'; 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './src/screens/LoginScreen';
+import SignUp from './src/screens/SignupScreen';
+
+import { enableScreens } from 'react-native-screens'; 
+
+enableScreens(); // 성능 향상 및 화면 전환 최적화
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  // 상태 관리: 사용자 입력값
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  // 로그인 버튼 클릭 시 처리 함수
-  const handleLogin = () => {
-    if (username === 'admin' && password === 'password') {
-      Alert.alert('Login Successful', 'You have logged in successfully!');
-    } else {
-      Alert.alert('Login Failed', 'Invalid username or password.');
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Login</Text>
-
-      {/* 사용자 이름 입력 필드 */}
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-
-      {/* 비밀번호 입력 필드 */}
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true} // 비밀번호는 숨기기
-      />
-
-      {/* 로그인 버튼 */}
-      <Button title="Login" onPress={handleLogin} />
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        {/* <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} /> */}
+         <Stack.Screen name="Login" component={Login}  />
+         <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-// 스타일링
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 40,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingLeft: 10,
-    borderRadius: 5,
-  },
-});
 
 export default App;

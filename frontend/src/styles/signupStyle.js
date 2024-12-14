d23e45d1b0ebf957a1ebb8876e3b39d1d74c6748
seoyunZZang <p815000@naver.com>
2024-12-15 03:46:48 +0900

    회원가입 style 수정

--- frontend/src/styles/signupStyle.js
+++ frontend/src/styles/signupStyle.js
@@ -3,13 +3,12 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
     justifyContent: 'center',
     alignItems: 'center',
   },
   subcontainer: {
     width : '70%',
-    height : '70%',
   },
   input: { 
     height: 50, 
@@ -17,16 +16,26 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1, 
     marginBottom: 20, 
   },
+
   subText: {
     fontSize: 13,
     marginBottom: 5,
     fontFamily : 'GmarketSansTTFLight',
   },
-  row: {
+
+  authrow: {
     flexDirection: 'row',  // 버튼을 가로로 배치
     justifyContent: 'space-between',  // 버튼 간의 간격을 균등하게 분배
-    width: '100%'
+    width: '90%',
+    marginTop: '10%',
   },
+  iconrow: {
+      flexDirection: 'row',  // 버튼을 가로로 배치
+      justifyContent: 'space-evenly',  // 버튼 간의 간격을 균등하게 분배
+      width: '90%',
+      marginBottom: 30,
+  },
+
   btn : {
     backgroundColor: '#656565',
     justifyContent: 'center',
@@ -54,7 +63,24 @@ const styles = StyleSheet.create({
   appName : {
     color : 'gray',
     fontFamily : 'GmarketSansTTFMedium',
-  }
+    marginBottom : 20,
+  },
+  line : {
+//      flex : 1,
+      borderBottomWidth : 1,
+      width : "40%",
+      borderBottomColor: '#EAEAEA',
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  icon : {
+    marginTop: 20,
+    alignItems: 'center'
+  },
+  authText : {
+      fontFamily : 'GmarketSansTTFMedium',
+      fontSize : 11,
+    },
 });
 
 export default styles;

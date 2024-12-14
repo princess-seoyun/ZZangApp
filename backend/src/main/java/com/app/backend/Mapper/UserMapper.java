7a4c01a6986285f0adc10d6fede600f282ba574d
seoyunZZang <p815000@naver.com>
2024-12-15 03:44:48 +0900

    개인정보 컬럼 추가

--- backend/src/main/java/com/app/backend/Mapper/UserMapper.java
+++ backend/src/main/java/com/app/backend/Mapper/UserMapper.java
@@ -8,11 +8,11 @@ import com.app.backend.Entity.JoinEntity;
 @Mapper
 public interface UserMapper {
 
-    @Insert("insert into cust_info (id, password, name, email, role) values (#{id}, #{password}, #{name}, #{email} , #{role})")
+    @Insert("insert into cust_info (id, password, name, email, role, gender, age , auth_path) values (#{id}, #{password}, #{name}, #{email} , #{role}, #{gender} , #{age}, #{auth_path})")
     int insert(JoinEntity userEntity);
 
 //  id 로 pw 가져오기
     @Select("SELECT password FROM cust_info WHERE id =#{id}")
-    String login(String id);
+    String getId(String id);
 
 }
